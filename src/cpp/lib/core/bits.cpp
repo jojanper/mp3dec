00d0ba87b1343a591b259635f81552e8f82d5ff3
Juha Ojanperä <juha.ojanpera@gmail.com>
2019-05-02 22:28:59 +0300

    Bitstream interface refactorings.

--- src/cpp/lib/core/bits.cpp
+++ src/cpp/lib/core/bits.cpp
@@ -25,84 +25,66 @@ static const uint32_t mask[] = {
     0x7F,       0xFF,        0x1FF,       0x3FF,       0x7FF,      0xFFF,      0x1FFFL,
     0x3FFFL,    0x7FFFL,     0xFFFFL,     0x1FFFFL,    0x3FFFFL,   0x7FFFFL,   0xFFFFFL,
     0x1FFFFFL,  0x3FFFFFL,   0x7FFFFFL,   0xFFFFFFL,   0x1FFFFFFL, 0x3FFFFFFL, 0x7FFFFFFL,
-    0xFFFFFFFL, 0x1FFFFFFFL, 0x3FFFFFFFL, 0x7FFFFFFFL, 0xFFFFFFFFL};
+    0xFFFFFFFL, 0x1FFFFFFFL, 0x3FFFFFFFL, 0x7FFFFFFFL, 0xFFFFFFFFL
+};
 
 
-Bit_Stream::Bit_Stream(void)
+Bit_Stream::Bit_Stream() :
+    m_ioBuf(NULL),
+    m_eobs(true),
+    m_buffer(NULL),
+    m_bufLen(0),
+    m_bitCounter(0),
+    m_bufIndex(0),
+    m_empty(false),
+    m_streamSize(0)
 {
-    bits_written = 0;
-    bit_buffer = NULL;
-    buf_len = 0;
-    bit_counter = 0;
-    buf_index = 0;
-    eobs = 1;
-    buffer_empty = 0;
-    streamSize = 0;
+    // bits_written = 0;
+    /*
+    m_buffer = NULL;
+    m_bufLen = 0;
+    m_bitCounter = 0;
+    m_bufIndex = 0;
+    m_eobs = 1;
+    mEmpty = false;
+    m_streamSize = 0;
+    */
 }
 
 Bit_Stream::~Bit_Stream(void)
 {
-    printf("desctructor\n");
-    if (bit_buffer) free(bit_buffer); // delete [] bit_buffer;
-    bit_buffer = NULL;
-    printf("desctructir done\n");
-
-    eobs = 1;
-    buf_len = 0;
-    bit_counter = 0;
-    buf_index = 0;
-    buffer_empty = 0;
-    streamSize = 0;
+    if (m_buffer)
+        free(m_buffer);
+    m_buffer = NULL;
 }
-
-
-/**************************************************************************
-  Title       : Open
-
-  Purpose     : Opens the bitstream.
-
-  Usage       : y = Open(hInst, stream, mode, size, sBuf)
-
-  Input       : hInst  - current instance handle
-                stream - name of the stream to be opened
-                mode   - READ_MODE, WRITE_MODE or APPEND_MODE
-                size   - size of the bitstream buffer (in units of 'uint32')
-                sBuf   - streaming buffer
-
-  Output      : y - TRUE on success, FALSE otherwise;
-                    throws AdvanceExcpt * on allocation failure
-
-  Explanation : When streaming, the 'stream' and 'size' parameters are ignored.
-
-  Author(s)   : Juha Ojanpera
-  ************************************************************************/
 
 bool
 Bit_Stream::open(StreamBuffer *ioBuf, int size)
 {
-    bit_buffer = NULL;
+    m_buffer = NULL;
     this->m_ioBuf = ioBuf;
 
+    m_bufIndex = 0;
     if (!this->m_ioBuf->CanWrite()) {
-        bit_counter = 0;
-        buf_index = -1;
+        m_bitCounter = 0;
+        // m_bufIndex = -1;
     }
     else {
-        buf_index = 0;
-        bit_counter = SLOT_BITS;
+        // buf_index = 0;
+        m_bitCounter = SLOT_BITS;
     }
 
     /*-- Allocate the bit buffer. --*/
-    buf_len_old = buf_len = size;
-    bit_buffer = (uint8_t *) calloc(1, size);
+    m_bufLenOrig = m_bufLen = size;
+    m_buffer = (uint8_t *) calloc(1, size);
     // new uint8_t[buf_len];
 
-    bits_written = 0;
-    buffer_empty = 1;
-    eobs = 0;
+    // bits_written = 0;
+    m_empty = 1;
+    m_eobs = 0;
 
     /*-- The size is needed when calculating the total length of the file. --*/
-    streamSize = this->m_ioBuf->GetStreamSize();
+    m_streamSize = this->m_ioBuf->GetStreamSize();
 
     return true;
 }
@@ -120,13 +102,15 @@ Bit_Stream::open(StreamBuffer *ioBuf, int size)
 void
 Bit_Stream::close(void)
 {
-    printf("Close\n");
-    if (this->m_ioBuf->CanWrite() && ((buf_index | bit_counter) != 0)) ff_buffer(1);
-    printf("close done %p\n", bit_buffer);
+    // printf("Close\n");
+    if (this->m_ioBuf->CanWrite() && ((m_bufIndex | m_bitCounter) != 0))
+        ff_buffer(1);
+    // printf("close done %p\n", m_buffer);
 
-    if (bit_buffer) free(bit_buffer); // delete [] bit_buffer;
-    bit_buffer = NULL;
-    printf("close done done\n");
+    if (m_buffer)
+        free(m_buffer); // delete [] m_buffer;
+    m_buffer = NULL;
+    // printf("close done done\n");
 }
 
 
@@ -152,17 +136,17 @@ void
 Bit_Stream::ff_buffer(int force_write)
 {
     if (!this->m_ioBuf->CanWrite()) {
-        if (!eobs) {
-            buf_index++;
-            if (buf_index == buf_len || buffer_empty) {
+        if (!m_eobs) {
+            m_bufIndex++;
+            if (m_bufIndex == m_bufLen || m_empty) {
                 /*
-                 * Update the file pointer before reading new bits to to bit
-                 * buffer. If 'buf_index' == 0 we are reading for the first
+                 * Update the file pointer before reading new bits to the bit
+                 * buffer. If 'm_bufIndex' == 0 we are reading for the first
                  * time so no need to update the pointer.
                  */
-                if (!buffer_empty && buf_index != 0)
-                    this->m_ioBuf->SeekBuffer(CURRENT_POS, buf_index);
-                auto nitems = this->m_ioBuf->ReadToBuffer(bit_buffer, buf_len);
+                if (!m_empty && m_bufIndex != 0)
+                    this->m_ioBuf->SeekBuffer(CURRENT_POS, m_bufIndex);
+                auto nitems = this->m_ioBuf->ReadToBuffer(m_buffer, m_bufLen);
 
                 if (nitems == 0) {
                     /*
@@ -170,38 +154,38 @@ Bit_Stream::ff_buffer(int force_write)
                      * the bitstream has been reached. This ensures that
                      * there should be no sudden changes at the output.
                      */
-                    eobs = 1;
-                    memset(this->bit_buffer, 0, buf_len);
+                    m_eobs = 1;
+                    memset(this->m_buffer, 0, m_bufLen);
                     return;
                 }
 
-                buf_index = 0;
-                buffer_empty = 0;
-                buf_len = (int) nitems;
+                m_bufIndex = 0;
+                m_empty = false;
+                m_bufLen = nitems;
             }
         }
     }
     else {
-        buf_index++;
+        m_bufIndex++;
 
         /* Check whether the buffer is full. */
-        if (buf_index == buf_len || force_write) {
+        if (m_bufIndex == m_bufLen || force_write) {
             /*
              * Check how much we actually need to save. If 'force_write'
              * is equal to one we might actually need to save only some
              * part of the buffer.
              */
-            buf_index = (buf_index == buf_len) ? buf_len : buf_index;
+            m_bufIndex = (m_bufIndex == m_bufLen) ? m_bufLen : m_bufIndex;
 
             /* Write the buffer to output stream. */
-            printf("WRITE: %i %i\n", buf_index, buf_len);
-            this->m_ioBuf->WriteFromBuffer(bit_buffer, buf_index);
-            printf("WRITE DONE\n");
+            // printf("WRITE: %i %i\n", buf_index, buf_len);
+            this->m_ioBuf->WriteFromBuffer(m_buffer, m_bufIndex);
+            // printf("WRITE DONE\n");
 
             // if(nitems != buf_index)
             //  ; // Add error handling here if needed.
 
-            buf_index = 0;
+            m_bufIndex = 0;
         }
     }
 }
@@ -215,9 +199,9 @@ Bit_Stream::putBits(int n, uint32_t word)
     while (n) {
         auto rbits = (n > SLOT_BITS) ? SLOT_BITS : n;
         n -= rbits;
-        printf("n = %i %i %i %i\n", rbits, n, bit_counter, buf_index);
+        // printf("n = %i %i %i %i\n", rbits, n, m_bitCounter, m_bufIndex);
         this->putbits8(rbits, ((word >> n) & mask[rbits]));
-        printf("putbits8 done %i\n", buf_index);
+        // printf("putbits8 done %i\n", buf_index);
     }
 }
 
@@ -240,58 +224,58 @@ Bit_Stream::putBits(int n, uint32_t word)
 void
 Bit_Stream::putbits8(int n, uint32_t word)
 {
-    bits_written += n;
+    // bits_written += n;
 
     /*-- Update the bitstream buffer index. --*/
-    if (bit_counter == 0) {
+    if (m_bitCounter == 0) {
         ff_buffer(0);
-        bit_counter = SLOT_BITS;
-        bit_buffer[buf_index] = 0;
+        m_bitCounter = SLOT_BITS;
+        m_buffer[m_bufIndex] = 0;
     }
 
     /*
-     *  If 'n' is bigger than 'bit_counter', 'word' must be splitted
+     *  If 'n' is bigger than 'm_bitCounter', 'word' must be splitted
      *  into two parts.
      *
      *  e.g. 'word' = 00000011111101010101010111111100 (= 16642095)
      *       'n' = 26;
-     *       'bit_counter' = 20
+     *       'm_bitCounter' = 20
      *
      *        000000  11111101010101010111        111100
      *                |<---upper part--->|  |<--lower part-->|
      */
-    if ((bit_counter - n) < 0) {
+    if ((m_bitCounter - n) < 0) {
         int end;
         uint32_t next;
 
-        printf("HIP\n");
+        // printf("HIP\n");
 
         /*-- Number of bits needed to store the lower part. --*/
-        end = n - bit_counter;
+        end = n - m_bitCounter;
 
         /*-- Store the upper part to the bitstream buffer. --*/
-        bit_buffer[buf_index] |= ((uint32_t) word >> end);
+        m_buffer[m_bufIndex] |= ((uint32_t) word >> end);
 
         /*-- Mask the upper part from 'word' to zero. --*/
         next = (uint32_t) word & mask[end];
 
         /*-- Update the bitstream buffer index. --*/
         ff_buffer(0);
-        bit_counter = SLOT_BITS;
-        bit_buffer[buf_index] = 0;
+        m_bitCounter = SLOT_BITS;
+        m_buffer[m_bufIndex] = 0;
 
-        bit_counter -= end;
+        m_bitCounter -= end;
 
         /*-- Store the lower part to the bitstream buffer. --*/
-        bit_buffer[buf_index] |= ((uint32_t) next << bit_counter);
+        m_buffer[m_bufIndex] |= ((uint32_t) next << m_bitCounter);
     }
     else {
-        printf("HOP %i\n", buf_index);
+        // printf("HOP %i\n", buf_index);
 
-        bit_counter -= n;
+        m_bitCounter -= n;
 
         /*-- For safety, mask the unwanted bits to zero. --*/
-        bit_buffer[buf_index] |= ((word & mask[n]) << bit_counter);
+        m_buffer[m_bufIndex] |= ((word & mask[n]) << m_bitCounter);
     }
 }
 
@@ -334,24 +318,24 @@ Bit_Stream::getbits8(int n)
     uint32_t tmp;
 
     /*-- Update the bitstream buffer index. --*/
-    if (bit_counter == 0) {
+    if (m_bitCounter == 0) {
         ff_buffer(0);
-        bit_counter = SLOT_BITS;
+        m_bitCounter = SLOT_BITS;
     }
 
-    idx = bit_counter - n;
+    idx = m_bitCounter - n;
     if (idx < 0) {
         /*-- Mask the unwanted bits to zero. --*/
-        tmp = (bit_buffer[buf_index] & mask[bit_counter]) << -idx;
+        tmp = (m_buffer[m_bufIndex] & mask[m_bitCounter]) << -idx;
 
         /*-- Update the bit stream buffer. --*/
         ff_buffer(0);
-        bit_counter = SLOT_BITS + idx;
-        tmp |= (bit_buffer[buf_index] >> bit_counter) & mask[-idx];
+        m_bitCounter = SLOT_BITS + idx;
+        tmp |= (m_buffer[m_bufIndex] >> m_bitCounter) & mask[-idx];
     }
     else {
-        bit_counter = idx; // bit_counter -= n;
-        tmp = (bit_buffer[buf_index] >> bit_counter) & mask[n];
+        m_bitCounter = idx; // m_bitCounter -= n;
+        tmp = (m_buffer[m_bufIndex] >> m_bitCounter) & mask[n];
     }
 
     return tmp;
@@ -381,19 +365,19 @@ Bit_Stream::skipbits8(int n)
     int idx;
 
     /*-- Update the bitstream buffer index. --*/
-    if (bit_counter == 0) {
+    if (m_bitCounter == 0) {
         ff_buffer(0);
-        bit_counter = SLOT_BITS;
+        m_bitCounter = SLOT_BITS;
     }
 
-    idx = bit_counter - n;
+    idx = m_bitCounter - n;
     if (idx < 0) {
         /*-- Update the bit stream buffer. --*/
         ff_buffer(0);
-        bit_counter = SLOT_BITS + idx;
+        m_bitCounter = SLOT_BITS + idx;
     }
     else
-        bit_counter = idx; // bit_counter -= n;
+        m_bitCounter = idx; // m_bitCounter -= n;
 }
 
 
@@ -423,7 +407,8 @@ Bit_Stream::skipBits(int n)
     for (i = 0; i < bytes; i++)
         skipbits8(SLOT_BITS);
 
-    if (bits_left) skipbits8(bits_left);
+    if (bits_left)
+        skipbits8(bits_left);
 }
 
 
@@ -444,8 +429,9 @@ Bit_Stream::byteAlign(void)
 {
     int bits_to_byte_align;
 
-    bits_to_byte_align = bit_counter & 7; // % 8;
-    if (bits_to_byte_align) skipbits8(bits_to_byte_align);
+    bits_to_byte_align = m_bitCounter & 7; // % 8;
+    if (bits_to_byte_align)
+        skipbits8(bits_to_byte_align);
 
     return bits_to_byte_align;
 }
@@ -473,11 +459,11 @@ Bit_Stream::lookAhead(int N)
     uint32_t dword;
 
     /*-- Store only the most important variables. --*/
-    bs_tmp.eobs = eobs;
-    bs_tmp.buf_len = buf_len;
-    bs_tmp.bit_counter = bit_counter;
-    bs_tmp.buf_index = buf_index;
-    bs_tmp.buffer_empty = buffer_empty;
+    bs_tmp.m_eobs = m_eobs;
+    bs_tmp.m_bufLen = m_bufLen;
+    bs_tmp.m_bitCounter = m_bitCounter;
+    bs_tmp.m_bufIndex = m_bufIndex;
+    bs_tmp.m_empty = m_empty;
 
     /*
      * Due to the implementation mode of bitstream parsing, there must be
@@ -493,7 +479,7 @@ Bit_Stream::lookAhead(int N)
      * Restore previous state of the bitstream buffer.
      * First check if bitstream buffer has been updated.
      */
-    if (buf_index < bs_tmp.buf_index) {
+    if (m_bufIndex < bs_tmp.m_bufIndex) {
         /*
          * Remember that the stream pointer has to be explicitly
          * updated. Due to this, the file pointer has been advanced
@@ -505,18 +491,18 @@ Bit_Stream::lookAhead(int N)
          * since we are in lookahead mode from the StreamBuffer point of view,
          * meaning that the previous buffer has not been released for other use.
          */
-        this->m_ioBuf->SeekBuffer(CURRENT_POS, -bs_tmp.buf_len);
-        buf_len = this->m_ioBuf->ReadToBuffer(bit_buffer, bs_tmp.buf_len);
+        this->m_ioBuf->SeekBuffer(CURRENT_POS, -bs_tmp.m_bufLen);
+        m_bufLen = this->m_ioBuf->ReadToBuffer(m_buffer, bs_tmp.m_bufLen);
     }
 
     /*-- Back to "normal" mode of operation. --*/
     this->m_ioBuf->SetLookAheadMode(false);
 
-    eobs = bs_tmp.eobs;
-    buf_len = bs_tmp.buf_len;
-    bit_counter = bs_tmp.bit_counter;
-    buf_index = bs_tmp.buf_index;
-    buffer_empty = bs_tmp.buffer_empty;
+    m_eobs = bs_tmp.m_eobs;
+    m_bufLen = bs_tmp.m_bufLen;
+    m_bitCounter = bs_tmp.m_bitCounter;
+    m_bufIndex = bs_tmp.m_bufIndex;
+    m_empty = bs_tmp.m_empty;
 
     return dword;
 }
@@ -535,16 +521,15 @@ Bit_Stream::lookAhead(int N)
   *************************************************************************/
 
 void
-Bit_Stream::FlushStream(void)
+Bit_Stream::flushStream(void)
 {
-    int byte_offset;
-
     byteAlign();
 
     /* Calculate how much the stream pointer needs to be updated. */
-    byte_offset = buf_index;
-    byte_offset += (SLOT_BITS - bit_counter) >> 3;
-    if (byte_offset) this->m_ioBuf->SeekBuffer(CURRENT_POS, byte_offset);
+    auto byte_offset = m_bufIndex;
+    byte_offset += (SLOT_BITS - m_bitCounter) >> 3;
+    if (byte_offset)
+        this->m_ioBuf->SeekBuffer(CURRENT_POS, byte_offset);
 
     /*
      * This has the effect that the next time we start to read the bit
@@ -552,13 +537,13 @@ Bit_Stream::FlushStream(void)
      * because if this function is called then it is assumed that the stream
      * pointer is already in its correct place.
      */
-    buffer_empty = 1;
-    buf_index = -1;
-    bit_counter = 0;
+    m_empty = true;
+    m_bufIndex = 0;
+    m_bitCounter = 0;
 }
 
 int32_t
-Bit_Stream::SeekStream(FilePos filePos, int32_t offset)
+Bit_Stream::seekStream(FilePos filePos, int32_t offset)
 {
     return this->m_ioBuf->SeekBuffer(filePos, offset);
 }
--- src/cpp/lib/include/core/bits.h
+++ src/cpp/lib/include/core/bits.h
@@ -13,12 +13,28 @@ public:
     Bit_Stream(void);
     ~Bit_Stream(void);
 
-    /*-- Public methods. --*/
+    /**
+     * Open and initialize bitstream.
+     *
+     * @param ioBuf Input stream handle
+     * @param size Bitstream buffer size
+     * @return true on success, false otherwise
+     */
     bool open(StreamBuffer *ioBuf, int size);
-    void close(void);
 
-    bool endOfStream() const { return eobs; }
+    /**
+     * Close bitstream.
+     */
+    void close();
 
+    /**
+     * Return true if end of bitstream found, false otherwise.
+     */
+    bool endOfStream() const { return m_eobs; }
+
+    /**
+     * Return specified number of bits from bitstream.
+     */
     uint32_t getBits(int n);
     void putBits(int n, uint32_t word);
     void skipBits(int n);
@@ -28,35 +44,40 @@ public:
 
     inline const uint8_t *getBuffer() const
     {
-        return &bit_buffer[buf_index + ((8 - bit_counter) >> 3)];
+        return &m_buffer[m_bufIndex + ((8 - m_bitCounter) >> 3)];
     }
 
-    int getSlotsLeft() const { return (buf_len - buf_index); }
+    int getSlotsLeft() const { return (m_bufLen - m_bufIndex); }
 
-    void AdvanceBufferSlot(int offset) { buf_index += offset; }
+    void advanceBufferSlot(int offset) { m_bufIndex += offset; }
 
-    uint32_t GetStreamSize() const { return streamSize; }
+    inline size_t getStreamSize() const { return m_streamSize; }
 
-    uint32_t GetCurrStreamPos()
+    uint32_t getCurrStreamPos()
     {
         uint32_t pos = this->m_ioBuf->SeekBuffer(CURRENT_POS, 0);
-        pos += buf_index + ((8 - bit_counter) >> 3);
+        pos += m_bufIndex + ((8 - m_bitCounter) >> 3);
         return pos;
     }
 
-    void ReleaseBufferLock() { eobs = 0; }
+    inline void releaseBufferLock() { m_eobs = 0; }
 
-    void SetBufferSize(uint32_t newSize)
+    inline bool adjustBufferSize(size_t newSize)
     {
-        buf_len_old = buf_len;
-        buf_len = newSize;
+        // m_bufLen2 = buf_len;
+        if (newSize < m_bufLenOrig) {
+            m_bufLen = newSize;
+            return true;
+        }
+
+        return false;
     }
 
-    void SetDefaultBufferSize() { buf_len = buf_len_old; }
+    inline void setDefaultBufferSize() { m_bufLen = m_bufLenOrig; }
 
     /*-- Public methods related to the seeking of the stream. --*/
-    void FlushStream();
-    int32_t SeekStream(FilePos filePos, int32_t offset);
+    void flushStream();
+    int32_t seekStream(FilePos filePos, int32_t offset);
 
 private:
     void skipbits8(int n);
@@ -66,14 +87,12 @@ private:
 
     /*-- Private parameters --*/
     StreamBuffer *m_ioBuf; // Name of file to be opened for bit parsing.
-    // int device_mode;            // File mode (READ, WRITE, APPEND).
-    uint32_t bits_written; // # bits written.
-    bool eobs;             // End of bitstream.
-    uint8_t *bit_buffer;   // Bitstream buffer.
-    int buf_len;           // Size of the bit buffer.
-    int bit_counter;       // Bit counter within bit slot.
-    int buf_index;         // Read/write index within bit buffer.
-    int buffer_empty;      // Buffer empty ?
-    uint32_t streamSize;   // Stream size in bytes.
-    uint32_t buf_len_old;  // Used for buffer reduction.
+    bool m_eobs;           // End of bitstream.
+    uint8_t *m_buffer;     // Bitstream buffer.
+    size_t m_bufLen;       // Size of the bit buffer.
+    int m_bitCounter;      // Bit counter within bit slot.
+    size_t m_bufIndex;     // Read/write index within bit buffer.
+    bool m_empty;          // Buffer empty ?
+    size_t m_streamSize;   // Stream size in bytes.
+    size_t m_bufLenOrig;   // Used for buffer reduction.
 };
