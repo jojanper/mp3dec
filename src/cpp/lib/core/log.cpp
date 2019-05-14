fe72e508b13c611fac414e56ca5ff5c87b626d44
Juha Ojanperä <juha.ojanpera@gmail.com>
2019-05-14 22:57:51 +0300

    Import logger.

---
+++ src/cpp/lib/core/log.cpp
@@ -0,0 +1,88 @@
+/**************************************************************************
+  log.cpp - Logfile implementations.
+
+  Author(s): Juha Ojanpera
+  Copyright (c) 1999 Juha Ojanpera.
+  *************************************************************************/
+
+/*-- System Headers. --*/
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*-- Project Headers. --*/
+#include "core/log.h"
+#include "core/throw.h"
+
+/*
+   Purpose:     Maximum length of a log message.
+   Explanation: - */
+const int MAXLOGLINELEN = 2048;
+
+
+LogFile::LogFile(const char *m_szLogFileArg) :
+    m_fpLog(NULL),
+    m_szLogFile(strdup(m_szLogFileArg)),
+    m_openCount(0)
+{}
+
+LogFile::~LogFile()
+{
+    this->close();
+
+    if (m_szLogFile)
+        free(m_szLogFile);
+}
+
+
+bool
+LogFile::open()
+{
+    m_openCount++;
+
+    if (m_fpLog)
+        return true;
+
+    m_fpLog = fopen(m_szLogFile, "a");
+    if (m_fpLog == NULL) {
+        char buf[256];
+
+        sprintf(buf, "Unable to open logfile %s", m_szLogFile);
+        throw new AdvanceExcpt(new ErrorMsg(buf));
+    }
+
+    return (m_fpLog != NULL);
+}
+
+bool
+LogFile::close()
+{
+    if (m_fpLog == NULL)
+        return false;
+
+    fclose(m_fpLog);
+    m_fpLog = NULL;
+
+    return true;
+}
+
+void
+LogFile::write(char *format, ...)
+{
+    va_list argptr;
+
+    if (m_fpLog == NULL)
+        return;
+
+    char *szBuffer = new char[MAXLOGLINELEN];
+
+    va_start(argptr, format);
+    vsprintf(szBuffer, format, argptr);
+    va_end(argptr);
+
+    fprintf(m_fpLog, "%s", szBuffer);
+    fflush(m_fpLog);
+
+    delete szBuffer;
+}
---
+++ src/cpp/lib/include/core/log.h
@@ -0,0 +1,38 @@
+/**************************************************************************
+  log.h - Class declaration for log messages.
+
+  Author(s): Juha Ojanpera
+  Copyright (c) 1999, 2019 Juha Ojanpera.
+  *************************************************************************/
+
+#ifndef LOG_H_
+#define LOG_H_
+
+/*-- System Headers. --*/
+#include <stdio.h>
+
+class LogFile
+{
+public:
+    LogFile(const char *szLogFileArg = "draal.log");
+    ~LogFile();
+
+    // Open logger for writing
+    bool open();
+
+    // Close logger
+    bool close();
+
+    // Writes messages to logfile
+    void write(char *Format, ...);
+
+    // Number of entities using the logger
+    inline int getOpenCount() const { return m_openCount; }
+
+private:
+    FILE *m_fpLog;
+    char *m_szLogFile;
+    int m_openCount;
+};
+
+#endif /* LOG_H_ */
